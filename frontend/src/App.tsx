7f2c30bb075c6a0441f3900566cdca0f9e853823
hsraktu17 <utkarshrajsrivastava7@gmail.com>
2024-03-12 12:17:39 +0530

    node_modeules removed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+
+function App(){
+  return <div>
+    <BrowserRouter>
+      <Routes>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
+  </div>
+}
+
+export default App;
